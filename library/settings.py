a6ae5c9852b95c8b55df3f79d58d9aa69d0d5443
saraelias7474 <sarah.elias2003@gmail.com>
2025-08-04 21:51:03 +0300

    fix: added .render.yaml and gunicorn

--- library/settings.py
+++ library/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+ALLOWED_HOSTS = ['*']
+DEBUG = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
